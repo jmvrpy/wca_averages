33eaee49122f304c824a06362ae703d7690ff82a
jmvrpy <jmvr@uvic.ca>
2026-08-28 15:26:36 -0700

    remove top 500 single restriction because some ppl for pyra arent in there lmao

--- statistics/abstract/average_of_x.rb
+++ statistics/abstract/average_of_x.rb
@@ -32,35 +32,6 @@ class AverageOfX < GroupedStatistic
         ON round_type.id = result.round_type_id
       JOIN result_attempts ra
         ON ra.result_id = result.id
-      JOIN (
-        SELECT event_id, person_id
-        FROM (
-          SELECT
-            person_best.event_id,
-            person_best.person_id,
-            RANK() OVER (
-              PARTITION BY person_best.event_id
-              ORDER BY person_best.best
-            ) AS world_rank
-          FROM (
-            SELECT
-              r.event_id,
-              r.person_id,
-              MIN(r.best) AS best
-            FROM results r
-            JOIN persons p
-              ON p.wca_id = r.person_id
-             AND p.sub_id = 1
-            WHERE r.best > 0
-              AND r.event_id NOT IN ('333mbf', '333mbo')
-            GROUP BY r.event_id, r.person_id
-          ) AS person_best
-        ) ranked_people
-        -- Take people from top 500 single for optimization reasons.
-        WHERE world_rank <= 500
-      ) top_people
-        ON top_people.event_id = result.event_id
-       AND top_people.person_id = result.person_id
       WHERE result.event_id NOT IN ('333mbf', '333mbo')
       ORDER BY competition.start_date, round_type.rank, ra.attempt_number
     SQL
