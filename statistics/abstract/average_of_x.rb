986ec03fdb14b717c03b4cb06e55acc61f7a617c
jmvrpy <jmvr@uvic.ca>
2026-09-02 10:08:23 -0400

    Update average_of_x.rb

--- statistics/abstract/average_of_x.rb
+++ statistics/abstract/average_of_x.rb
@@ -56,7 +56,7 @@ class AverageOfX < GroupedStatistic
             GROUP BY r.event_id, r.person_id
           ) AS person_best
         ) ranked_people
-        WHERE world_rank <= 1000
+        WHERE world_rank <= 5000
       ) top_people
         ON top_people.event_id = result.event_id
        AND top_people.person_id = result.person_id
@@ -90,7 +90,7 @@ class AverageOfX < GroupedStatistic
         end
         .reject { |person_link, best_aox, best_aox_solves| best_aox == SolveTime::DNF }
         .sort_by! { |person_link, best_aox, best_aox_solves| best_aox }
-        .first(200)
+        .first(500)
         .map do |person_link, best_aox, best_aox_solves|
           solve_times = best_aox_solves.map do |solve|
             solve == Float::INFINITY ? SolveTime::DNF : SolveTime.new(event_id, :single, solve)
